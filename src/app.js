d63455d9e1d8d848ab5366de234ff4690ffc6143
Lidia <lidiayang104@gmail.com>
2023-07-07 14:01:04 +0200

    saludo test

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('holllaa test')
+});
+
 app.use('/api', require('./routes/api'))
+
+
 
 
 module.exports = app;
